b36df8ecb31cecb5c9e0b925145e005e74b84660
solo samurai <ahmed_abdikani@hotmail.com>
2023-12-17 14:19:33 +0300

    lining setup prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  useTabs: true,
+  printWidth: 100,
+  endOfLine: "auto",
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+};
